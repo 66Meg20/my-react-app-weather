3c74c79458d7126a9170f9be7e6a0c004384e731
66Meg20 <66meg.dj2066@gmail.com>
2022-12-16 23:41:15 -0600

    Added the styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Weather defaultCity="amarillo" />
       </div>
       <div className="projectLink">
-        This project was coded by Megan Jackson and is open-sourced on{" "}
+        This project was coded by Megan Jackson, and is open-sourced on{" "}
         <a
           href="https://github.com/66Meg20/my-react-app-weather"
           target="_blank"
